c0d4f4f975d018d688a785d786aa08f3631afea4
bobbykim89 <bobby.sigun.kim@gmail.com>
2021-12-28 21:46:59 -0700

    initializing with vuetifty and webpack

---
+++ src/layouts/Default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-toolbar-title>
+        E-vents
+      </v-toolbar-title>
+    </v-app-bar>
+    <p>{{ $static.metadata.siteDescription }}</p>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
