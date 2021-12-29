004af44c4c2bcf6caf3a3fb2f864e18cebdea02e
bobbykim89 <bobby.sigun.kim@gmail.com>
2021-12-29 00:11:09 -0700

    created basic layout and fetching data from api, have some issue so need to fix it

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,6 +4,7 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const axios = require("axios");
 
 module.exports = function(api) {
   api.chainWebpack((config, { isServer }) => {
@@ -16,11 +17,24 @@ module.exports = function(api) {
     }
   });
 
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  });
+  api.loadSource(async (actions) => {
+    const { data } = await axios.get("http://localhost:1337/api/events");
 
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
+    const collection = actions.addCollection({
+      typeName: "Event",
+    });
+
+    for (const event of data) {
+      collection.addNode({
+        id: event.attributes.id,
+        title: event.attributes.title,
+        description: event.attributes.description,
+        price: event.attributes.price,
+        date: event.attributes.date,
+        duration: event.attributes.duration,
+        thumbnail: event.attributes.image.formats.thumbnail.url,
+        image: event.attributes.image.formats.medium.url,
+      });
+    }
   });
 };
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,11 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar>
+    <v-app-bar app>
       <v-toolbar-title>
         E-vents
       </v-toolbar-title>
+      <v-text-field
+        placeholder="Search"
+        class="ml-8"
+        style="max-width: 350px;"
+        prepend-inner-icon="mdi-magnify"
+        outlined
+        rounded
+        dense
+        hide-details
+      />
+      <v-spacer />
+      <v-btn>Create Event</v-btn>
     </v-app-bar>
-    <p>{{ $static.metadata.siteDescription }}</p>
     <v-main>
       <slot />
     </v-main>
